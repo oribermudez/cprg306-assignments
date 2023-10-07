54afb3bd53fbf3b0cfc1334bd6df59846843eb34
Ori Bermudez <ori.bermudez@outlook.com>
2023-10-06 23:02:08 -0600

    Improved list items style

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -1,40 +1,47 @@
 "use client"
-
 import { useState } from "react";
-
 import Item from "./item";
-import items from "./items.json"
+import items from "./items.json";
 
 const ItemList = () => {
   const [sortBy, setSortBy] = useState('name');
 
   return (
-    <>
-     <div className="text-gray-600 flex justify-center items-center w-80 lg:w-96">
-      <p className="mr-6 pt-4">Sort by</p>
-      <div className="flex justify-center items-center gap-4 lg:gap-6">
-        <button
-          onClick={() => setSortBy('name')}
-          className={`mt-7 inline-block rounded-md border border-transparent ${sortBy === 'name' ? 'bg-cyan-600 underline underline-offset-4' : 'bg-cyan-400'} px-8 py-3 text-center font-medium text-white hover:bg-cyan-500`}
-        >
-          Name
-        </button>
-        <button
-        onClick={() => setSortBy('category')}
-          className={`mt-7 inline-block rounded-md border border-transparent ${sortBy === 'category' ? 'bg-cyan-600 underline underline-offset-4' : 'bg-cyan-400'} px-8 py-3 text-center font-medium text-white hover:bg-cyan-500`}
-        >
-          Category
-        </button>
+    <div className="container mx-auto py-8">
+      <div className="flex items-center justify-between mb-6">
+        <p className="text-gray-600 text-lg">Sort by:</p>
+        <div className="flex space-x-4">
+          <button
+            onClick={() => setSortBy('name')}
+            className={`px-4 py-2 rounded-md ${sortBy === 'name' ? 'bg-cyan-500 text-white' : 'bg-cyan-200 text-gray-800 hover:bg-cyan-300 hover:text-white'} focus:outline-none transition duration-300`}
+          >
+            Name
+          </button>
+          <button
+            onClick={() => setSortBy('category')}
+            className={`px-4 py-2 rounded-md ${sortBy === 'category' ? 'bg-cyan-500 text-white' : 'bg-cyan-200 text-gray-800 hover:bg-cyan-300 hover:text-white'} focus:outline-none transition duration-300`}
+          >
+            Category
+          </button>
+        </div>
       </div>
+      <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {items
+          .sort((a, b) => a[sortBy].localeCompare(b[sortBy]))
+          .map((item, index) => {
+            return (
+              <Item
+                key={`${index}-${item.quantity}`}
+                name={item.name}
+                quantity={item.quantity}
+                category={item.category}
+                icon={item.icon}
+              />
+            );
+          })}
+      </ul>
     </div>
-    <ul className="flex flex-col mt-6 w-80 lg:w-96">
-      {items.sort((a,b) => a[sortBy].localeCompare(b[sortBy])).map((item, index) => {
-        return <Item key={`${index}-${item.quantity}`} name={item.name} quantity={item.quantity} category={item.category} icon={item.icon}/>
-      })}
-    </ul>
-    </>
-    
   );
-}
+};
 
 export default ItemList;
